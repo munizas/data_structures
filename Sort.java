d7f55c31c3df2b613295662c5816690016370533
Agustin Muniz <gus.s.muniz@gmail.com>
2015-05-26 00:09:53 -0700

    implemenation of quick sort

--- Sort.java
+++ Sort.java
@@ -33,4 +33,39 @@ public class Sort {
         // copy sorted array into destination array
         System.arraycopy(aa, 0, a, p, k);
     }
+    
+    public static void qSort(int[] a, int p, int q) {
+        // check if sequence has at least 2 elements
+        if (q - p < 2)
+            return;
+        // compute the partition index
+        int index = partition(a, p, q);
+        // sort left half
+        qSort(a, p, index);
+        // sort right half
+        qSort(a, index+1, q);
+    }
+    
+    public static int partition(int[] a, int p, int q) {
+        // save pivot value
+        int pivot = a[p];
+        int i = p, j = q;
+        while (i < j) {
+            // find next j where a[j] is less than pivot
+            while (j > i && a[--j] >= pivot)
+                ;
+            // move smaller value to the left segment
+            if (j > i)
+                a[i] = a[j];
+            // find next i where a[i] is greater than pivot
+            while (i < j && a[++i] <= pivot)
+                ;
+            // move larger value to the left segment
+            if (i < j)
+                a[j] = a[i];
+        }
+        // place pivot in partition index
+        a[j] = pivot;
+        return j;
+    }
 }
